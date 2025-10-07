0e88d4de1018035671c9ffa73b2400ace1440e86
Aniket <148300120+Aniketsy@users.noreply.github.com>
2025-10-07 19:16:24 +0530

    Update Basic_points .py

--- Study_Material/Basic_points .py
+++ Study_Material/Basic_points .py
@@ -7,3 +7,11 @@ for num in nums:
 for key, value in freq.items():
     if value == 1:
         return key
+
+
+###############
+from collections import Counter
+freq = Counter(nums)   # Count all numbers efficiently
+for num, count in freq.items():
+    if count == 1:
+         return num
