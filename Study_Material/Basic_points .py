5bd583426dafd43ff022d84ed26ea7c3259db5fa
Aniket <148300120+Aniketsy@users.noreply.github.com>
2025-10-18 23:27:00 +0530

    Implement Pythagorean theorem formulas
    Added formulas for Pythagorean triples based on odd and even values of n.

--- Study_Material/Basic_points .py
+++ Study_Material/Basic_points .py
@@ -15,3 +15,87 @@ freq = Counter(nums)   # Count all numbers efficiently
 for num, count in freq.items():
     if count == 1:
          return num
+
+################## Formula for pythogores theorem
+Case 1: n is odd
+if n % 2 == 1:
+    b = (n * n - 1) // 2
+    c = (n * n + 1) // 2
+
+
+When n is odd, a Pythagorean triple formula is:
+
+(
+𝑎
+,
+𝑏
+,
+𝑐
+)
+=
+(
+𝑛
+,
+𝑛
+2
+−
+1
+2
+,
+𝑛
+2
++
+1
+2
+)
+(a,b,c)=(n,
+2
+n
+2
+−1
+	​
+
+,
+2
+n
+2
++1
+	​
+
+)
+
+Case 2: n is even
+else:
+    k = n // 2
+    b = k * k - 1
+    c = k * k + 1
+
+
+When n is even, define k = n / 2. Then the formula is:
+
+(
+𝑎
+,
+𝑏
+,
+𝑐
+)
+=
+(
+𝑛
+,
+𝑘
+2
+−
+1
+,
+𝑘
+2
++
+1
+)
+(a,b,c)=(n,k
+2
+−1,k
+2
++1)
